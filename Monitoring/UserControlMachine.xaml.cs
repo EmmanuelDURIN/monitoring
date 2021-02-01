3b55e6a96540ca77cf52b33ce0c726f42e8579fc
emmanuel <orsys@MW6100>
2021-02-01 16:55:48 +0100

    Add modal Window

---
+++ Monitoring/UserControlMachine.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Monitoring
+{
+    /// <summary>
+    /// Interaction logic for UserControlMachine.xaml
+    /// </summary>
+    public partial class UserControlMachine : UserControl
+    {
+        public UserControlMachine()
+        {
+            InitializeComponent();
+        }
+    }
+}
